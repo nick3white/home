9471c4508cb2c42f6556cad5a4fb6ff2bb9051d3
nick3white <nick33white@gmail.com>
2023-10-15 21:11:25 -0500

    move idx

--- docs/build/q-2b14568d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./q-ba2b7e5d.js";const o=()=>r(()=>import("./q-8ea06850.js"),[]);export{o as ServiceworkerServiceWorker};
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-        // base: '/home',
+        base: '/home',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
