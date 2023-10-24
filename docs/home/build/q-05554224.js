1a3f328715f873c9235c70f0bfa62b3da69bd1ef
Nick White <nick33white@gmail.com>
2023-10-24 11:21:03 -0500

    adding docs

---
+++ docs/home/build/q-05554224.js
@@ -0,0 +1,1 @@
+import{S as l,a as u}from"./q-28a3e3d2.js";const r=n=>l("form",null,{action:u(t=>t.search[0],[n]),method:"get"},[l("input",null,{name:"q"},null,3,null),l("button",null,{type:"submit"},u(t=>t.search[1],[n]),3,null)],3,"id_0");export{r as s_RQjhsJ90NAg};
--- docs/home/build/q-9523b0bf.js
+++
@@ -1,1 +0,0 @@
-import{C as r,a as s}from"./q-090eedd9.js";const t=()=>r(s,null,3,"yB_0");export{t as s_VKFlAWJuVm8};
